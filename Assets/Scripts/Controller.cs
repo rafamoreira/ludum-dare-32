bd8b1898f2cf39a87e1182b59703e5c3fbf02716
Rafael Moreira <i@rafaelmoreira.net>
2015-04-18 16:20:40 -0300

    add player controls

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-	
+    float playerSpeed = 10;
+
+	void Update () 
+    {
+        Move();
+        Rotate();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void Move()
+    {
+        if(Input.GetKey("up"))
+        {
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0, Space.World);
+        }
+
+        if(Input.GetKey("down"))
+        {
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0, Space.World);
+        }
+
+        if(Input.GetKey("left"))
+        {
+            transform.Translate(-playerSpeed * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        if(Input.GetKey("right"))
+        {
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0, Space.World);
+        }
+    }
+
+    void Rotate()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector3 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - Camera.main.transform.position.z));
+
+        Vector3 euler = transform.eulerAngles;
+        euler.z = Mathf.Atan2((screenPos.y - transform.position.y), (screenPos.x - transform.position.x)) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = euler;
+    }
 }
