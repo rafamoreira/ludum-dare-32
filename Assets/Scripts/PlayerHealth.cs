7e2d209bd847594a7db59f33a19d7249fd26262d
Rafael Moreira <i@rafaelmoreira.net>
2015-04-20 18:53:30 -0300

    invunerable after hit

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,23 +11,33 @@ public class PlayerHealth : MonoBehaviour
     public Image heart3;
     public Sprite heartFull;
     public Sprite heartEmpty;
+    SpriteRenderer spriteRenderer;
+    Controller playerController;
 	
     // Use this for initialization
 	void Start () 
     {
         health = 3;
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        playerController = GetComponent<Controller>();
 	}
 	
     public void TakeHitPlayer()
     {
         health -= 1;
+        if(health >= 0)
+        {
+            StartCoroutine("PlayerHit");
+        }
     }
 
     void Update()
     {
         if (health < 0)
             DeathRoutine();
+
+        //Debug.Log(playerLayer);
     }
 
     void DeathRoutine()
@@ -63,6 +73,35 @@ public class PlayerHealth : MonoBehaviour
             heart3.sprite = heartEmpty;
         }
         //healthGUI.text = "Health: " + health.ToString();
+    }
+
+    IEnumerator PlayerHit()
+    {
+        Physics2D.IgnoreLayerCollision(10, 8, true);
+        playerController.playerSpeed = 4;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.enabled = true;
+        playerController.playerSpeed = 2;
+        Physics2D.IgnoreLayerCollision(10, 8, false);
+        yield return null;
     }
 
 
