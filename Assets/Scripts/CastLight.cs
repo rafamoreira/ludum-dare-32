0077f6bf81f0c2c1c446d0a13350d64cac7eb283
Rafael Moreira <i@rafaelmoreira.net>
2015-04-19 23:17:29 -0300

    variable light length

--- Assets/Scripts/CastLight.cs
+++ Assets/Scripts/CastLight.cs
@@ -6,22 +6,27 @@ public class CastLight : MonoBehaviour
 
     public LayerMask mask;
 
+    int rayLenghtDefault = 4;
 
+    LineRenderer lightRenderer;
     LightDetect lightDetect;
 
 	// Use this for initialization
 	void Start () {
         lightDetect = GetComponentInParent<LightDetect>();
+        lightRenderer = GetComponentInChildren<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (lightDetect.onLight)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, Mathf.Infinity, mask);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, rayLenghtDefault, mask);
 
             if (hit.collider != null)
             {
+                lightRenderer.SetPosition(1, new Vector3(0, 0, hit.distance * 2));
+                Debug.Log(hit.distance);
                 Debug.Log(hit.collider.name);
             }
         }
