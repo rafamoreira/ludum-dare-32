56016888ea95bb416ddf7747181b5fb4490ce2b5
Rafael Moreira <i@rafaelmoreira.net>
2015-05-11 17:02:04 -0300

    change on raycast

--- Assets/Scripts/CastLight.cs
+++ Assets/Scripts/CastLight.cs
@@ -8,29 +8,40 @@ public class CastLight : MonoBehaviour
     int rayLenghtDefault = 4;
     LineRenderer lightRenderer;
     LightDetect lightDetect;
+	float lastPos;
 
 	// Use this for initialization
 	void Start () {
+		lastPos = 0;
         lightDetect = GetComponentInParent<LightDetect>();
         lightRenderer = GetComponentInChildren<LineRenderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if (lightDetect.onLight)
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, rayLenghtDefault, mask);
 
             if (hit.collider != null)
             {
+				//Debug.DrawRay(transform.position, transform.forward, Color.magenta, 0.01f);
+
+				//Debug.Log(Mathf.Abs(hit.point.y - transform.position.y));
                 lightRenderer.SetPosition(1, new Vector3(0, 0, hit.distance * 2));
+
                 if(hit.collider.tag == "Enemy")
                 {
                     hit.collider.SendMessage("TakeHit");
                 }
             }
             else
-                lightRenderer.SetPosition(1, new Vector3(0, 0, 8));
+			{
+				lightRenderer.SetPosition(1, new Vector3(0, 0, 8));
+
+			}
+
+            
         }
 	}
 }
